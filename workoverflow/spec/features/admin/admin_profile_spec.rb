377e23e5bf71e1abfe554a3169513f6276e9eecf
Joe Timmer & Neel Shah <pair+jtimmer89.nshah20@devbootcamp.com>
2014-10-09 20:51:16 -0500

    end of the day. still working on profile page

--- workoverflow/spec/features/admin/admin_profile_spec.rb
+++ workoverflow/spec/features/admin/admin_profile_spec.rb
@@ -2,17 +2,25 @@ require 'spec_helper'
 
 describe "Admin viewing" do
   context "profile" do
+
+    before(:each) do
+      @user1 = User.create!(name: "Bob Swinsong", email: "bob@gmail.com", password: "12345", password_confirmation: "12345")
+      @user2 = User.create!(name: "Susan Swinsong", email: "susan@gmail.com", password: "12345", password_confirmation: "12345")
+      @project1 = Project.create!(title: "Testing with RSpec", description: "Testing with the latest and greatest. I want to learn with friends", category: "Testing", location: "Chicago", remote: false, time_estimation: 5, creator_id: @user1.id)
+      @project2 = Project.create!(title: "AJAXifying Stuff", description: "Learning how to make things way fresh.", category: "Javascript", location: "San Francisco", remote: true, time_estimation: 7, creator_id: @user2.id)
+      Commitment.create!(user_id: @user1.id, project_id: @project2.id)
+      Commitment.create!(user_id: @user2.id, project_id: @project1.id)
+    end
+
     it "sees a list of all their projects" do
-
-      # @user = Sample User
-      # user authentication
-      # visit user_path(@user)
-      # expect page to list all of user projects
-
+      # login(@user)
+      visit user_path(@user1)
+      expect(page).to have_content @project1.title
     end
 
     it "sees a list of all their commitments" do
-      pending
+      visit user_path(@user1)
+      expect(page).to have_content @project2
     end
 
     it "can link to individual projects" do
