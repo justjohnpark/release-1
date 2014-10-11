e1426a043e814caf332397cd57b04df403154fdc
Joe Timmer & simensta <pair+jtimmer89.simensta@devbootcamp.com>
2014-10-11 17:04:52 -0500

    fixed bugs with ajaxed voting button ids

--- workoverflow/app/assets/javascripts/vote/vote.js
+++ workoverflow/app/assets/javascripts/vote/vote.js
@@ -1,9 +1,16 @@
 $(function(){
   console.log("page loaded")
-  $("form[data-remote=true]").on("ajax:success", function(event, data, status, xhr){
-    console.log(data.count);
+    $(".comment-block form").on("ajax:success", function(event, data, status, xhr){
+    console.log(event.target)
+    var $clicked = $(this)
+    $clicked.siblings('span.vote-visual-comment').html(data.count)
+  })
+
+
+  $(".project-vote-block form").on("ajax:success", function(event, data, status, xhr){
     $("span.vote-visual").html(data.count)
   })
+
 })
 
 
--- workoverflow/app/controllers/votes_controller.rb
+++ workoverflow/app/controllers/votes_controller.rb
@@ -1,27 +1,25 @@
 class VotesController < ApplicationController
   def comment_create
-
+    Vote.create(voteable_id: params[:id], voteable_type: "Comment", user_id: session[:user_id])
+      @comment = Comment.find_by_id(params[:id])
+      puts "__________Comment___________________________________________"
+      p @comment
+      puts "____________"
+      p @comment.id
+      respond_to do |format|
+        format.json { render json: {count: @comment.votes.count}}
+        format.html { redirect_to admins_project_path(@comment.project)}
+      end
   end
 
 
   def project_create
-    Vote.create(voteable_id: params[:id], voteable_type: "Project",user_id: session[:user_id])
+    Vote.create(voteable_id: params[:id], voteable_type: "Project", user_id: session[:user_id])
     @project = Project.find_by_id(params[:id])
     respond_to do |format|
-      # puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-      # puts "I am in the format block"
       format.json { render json: {count: @project.votes.count}}
       format.html { redirect_to admins_project_path(@project)}
     end
-    puts "I am at the end of the actions"
   end
-
-
-  if xhr.request?
-    do this
-
-  else
-
-
 
 end
