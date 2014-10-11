9e7f82c642b338d024d67ab7a9ae20ac096c05c6
Joe Timmer & simensta <pair+jtimmer89.simensta@devbootcamp.com>
2014-10-11 17:54:49 -0500

    appending comment contents with js

--- workoverflow/app/assets/javascripts/comment/comment.js
+++ workoverflow/app/assets/javascripts/comment/comment.js
@@ -1,8 +1,18 @@
 $(function(){
   console.log("loaded page")
-    $(".new-comment-block form").on("ajax:success", function(event, data, status, xhr){
-      console.log(data)
+    $(".new-comment-block form")
+    .on("ajax:beforeSend", function(){$(this).hide()})
+    .on("ajax:success", function(event, data, status, xhr){
+      $("body").append("<div class="comment-block">
+  <%= comment.content %><br>
+  <%= comment.user.name %><br>
+  <% if comment.user.id == @user.id %>
 
+  <span class="vote-visual-comment" id="<%= index %>">  <%= comment.votes.length %>  </span>
+
+  <%= button_to "Like Comment", comment_vote_path(comment) + '.json', "data-type" => :json, method: :post, class: "comment-project", remote: true %>
+
+  <%= link_to "Delete", admins_project_comment_path(@project, comment), method: :delete, confirm: "Are you sure?" %>
+</div>");
   })
-
 })
--- workoverflow/app/controllers/comments_controller.rb
+++ workoverflow/app/controllers/comments_controller.rb
@@ -1,9 +1,9 @@
 class CommentsController < ApplicationController
   def create
     @project = Project.find_by_id(params[:project_id])
-    @project.comments.create!(content: params[:comment][:content], user_id: session[:user_id])
+    comment = @project.comments.create!(content: params[:comment][:content], user_id: session[:user_id])
     respond_to do |format|
-        # format.json { render json: {comment: @comment}}
+        format.json { render json: {comment: comment}}
         format.html { redirect_to admins_project_path(@project)}
       end
   end
