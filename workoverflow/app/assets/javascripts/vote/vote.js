76527b3b2509082f2d9981c815733ad10b8ae7f4
Joe Timmer & simensta <pair+jtimmer89.simensta@devbootcamp.com>
2014-10-11 17:16:15 -0500

    check for comment id when voting

--- workoverflow/app/assets/javascripts/vote/vote.js
+++ workoverflow/app/assets/javascripts/vote/vote.js
@@ -6,7 +6,6 @@ $(function(){
     $clicked.siblings('span.vote-visual-comment').html(data.count)
   })
 
-
   $(".project-vote-block form").on("ajax:success", function(event, data, status, xhr){
     $("span.vote-visual").html(data.count)
   })
