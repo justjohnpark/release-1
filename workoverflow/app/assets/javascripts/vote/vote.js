ef664668e9a860bd41a93cf95ae5bba1482aa0b5
Joe Timmer & simensta <pair+jtimmer89.simensta@devbootcamp.com>
2014-10-11 12:22:44 -0500

    voting ajax without html update

--- workoverflow/app/assets/javascripts/vote/vote.js
+++ workoverflow/app/assets/javascripts/vote/vote.js
@@ -1,8 +1,10 @@
 $(function(){
   console.log("page loaded")
-  $("a[data-remote=true].vote-project").on("ajax:sucess", function(event){
-    event.preventDefault();
-    console.log("event prevented!!!")
+  $("a[data-remote=true].vote-project").on("ajax:success", function(event, data){
+    console.log(event.target)
+    console.log(data)
+
+
   })
 
 })
--- workoverflow/app/controllers/votes_controller.rb
+++ workoverflow/app/controllers/votes_controller.rb
@@ -5,9 +5,18 @@ class VotesController < ApplicationController
 
 
   def project_create
+
     puts "______________PARAMS______________"
     puts params
     Vote.create(voteable_id: params[:id], voteable_type: "Project",user_id: session[:user_id])
+    @project = Project.find_by_id(params[:id])
+    respond_to do |format|
+      format.html { redirect_to admins_project_path(@project)}
+      format.json { render json: {count: @project.votes.count}}
+      format.any do
+        render nothing: true
+      end
+    end
   end
 
 end
