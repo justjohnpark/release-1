bcc6654b00d12e10a9c0dfa54ab3841893e4c838
Joe Timmer & simensta <pair+jtimmer89.simensta@devbootcamp.com>
2014-10-12 12:13:06 -0500

    ajax comment box

--- workoverflow/app/assets/javascripts/comment/comment.js
+++ workoverflow/app/assets/javascripts/comment/comment.js
@@ -1,9 +1,8 @@
 $(function(){
-    $(".new-comment-block form").on("ajax:success", function(event, data, status, xhr){
+    $(".new-comment-block form").on("ajax:complete", function(event, data, status, xhr){
       $(this).hide();
-
-      $("#comment-placement").empty().append("<%= j render partial: 'layouts/comment', locals:{project: data.project} %>");
       $("div.new-comment-block textarea").val("");
+      $("#comment-placement").html(data.responseText)
       $(this).show();
    });
  });
