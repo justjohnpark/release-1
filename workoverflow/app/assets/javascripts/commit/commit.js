e3356690e7049f351355f6f088e886ed5752fd7b
“Joseph <joe.timmer89@gmail.com>
2014-10-12 18:29:45 -0500

    fixed commit ajax stuff

--- workoverflow/app/assets/javascripts/commit/commit.js
+++ workoverflow/app/assets/javascripts/commit/commit.js
@@ -1,4 +1,5 @@
 $(function() {
   $(".project_commit").on("ajax:success", function(e) {
+  	$("#commit-link").append("<h3 class='section-subheading text-muted'>You have committed to this project</h3>")
   });
 });
