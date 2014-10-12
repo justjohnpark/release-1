682634df27bce000ffa5daa3b4aae19895f5d8af
“Joseph <joe.timmer89@gmail.com>
2014-10-12 18:54:02 -0500

    commit js fixed and footer with joe

--- workoverflow/app/assets/javascripts/commit/commit.js
+++ workoverflow/app/assets/javascripts/commit/commit.js
@@ -1,5 +1,8 @@
 $(function() {
   $(".project_commit").on("ajax:success", function(e) {
-  	$("#commit-link").append("<h3 class='section-subheading text-muted'>You have committed to this project</h3>")
+		$("#commit-link").append("<h3 class='section-subheading text-muted'>You have committed to this project</h3>");
+  });
+  $(".project_commit").on("ajax:error", function(e) {
+		$("#commit-link").append("<h3 class='section-subheading text-muted'>You have already committed to this project</h3>");
   });
 });
